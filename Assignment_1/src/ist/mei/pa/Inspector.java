ab7b716d8a09f27658c3f0f5ce7fcce45bdc0d32
pafalium <pedroalfaiate1992@hotmail.com>
2014-03-13 11:37:32 +0000

    Cópia do trabalho feito no fork do Tomás.

--- Assignment_1/src/ist/mei/pa/Inspector.java
+++ Assignment_1/src/ist/mei/pa/Inspector.java
@@ -2,7 +2,18 @@ package ist.mei.pa;
 
 public class Inspector {
 	public void inspect(Object o) {
-		//TODO do something inteligent
-		//somthing more
+		// TODO do something inteligent
+		// somthing more
 	}
+	/*
+	  void terminate(){
+	  
+	  } void getValue(String name){
+	  
+	  } void modifie(String name, int value){
+	  
+	  } void callsMethod(String name, ){
+	  
+	  }
+	 */
 }
