7a750e8aa6b2f95248104cd2430fbd15e528c59b
pafalium <pedroalfaiate1992@hotmail.com>
2014-03-19 17:57:39 +0000

    Added a simple class for testing.

---
+++ Assignment_1/src/ist/mei/pa/test/TestClass.java
@@ -0,0 +1,15 @@
+package ist.mei.pa.test;
+
+import ist.mei.pa.Inspector;
+
+public class TestClass {
+	public static void main(String[] args) throws Exception {
+		Inspector inspector = new Inspector();
+		TestClass obj = new TestClass();
+		inspector.inspect(obj);
+	}
+	private void meth() {
+		// TODO Auto-generated method stub
+
+	}
+}
