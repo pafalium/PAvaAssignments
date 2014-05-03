7e7d59b216fb6bda02d2744c6bf7e95ecf1f53b1
pafalium <pedroalfaiate1992@hotmail.com>
2014-05-03 13:38:43 +0100

    Add more tests.

---
+++ Assignment_2/src/test/Test1.java
@@ -0,0 +1,29 @@
+package test;
+
+import ist.meic.pa.Trace;
+
+class Test1Aux {
+	public Object foo() {
+		return new String("Foo");
+	}
+	
+	public Object bar() {
+		return foo();
+	}
+	
+	public Object baz() {
+		return bar();
+	}
+	
+	public void test() {
+		Trace.print(foo());
+		Trace.print(bar());
+		Trace.print(baz());
+	}
+}
+
+public class Test1 {
+	public static void main(String args[]) {
+		(new Test1Aux()).test();
+	}
+}
